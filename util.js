d98080a3bb38cafbded16c11805b9cb910f0ed7b
emmeiwhite <emmeiwhite@gmail.com>
2018-10-29 13:33:38 +0530

    Adding item to list mutably

--- util.js
+++ util.js
@@ -1,5 +1,17 @@
+// 1.Sum of two numbers
 const sum = (num1,num2)=>{
     return num1+num2;
 }
 
-module.exports = {
+
+// 2. Adding Item to a list
+const addItemToList = (list,listItem)=>{
+    // a) In a mutable way we are adding an item to a list
+    list.push(listItem);
+    return list;
+}
+
+module.exports = {
+    // sum
+    addItemToList
+}
--- util.test.js
+++ util.test.js
@@ -1,7 +1,26 @@
 let util =require('./util.js'); // All util file comes here 
 
+
+// 1.Adding two numbers
+/*
 test('ADDING TWO NUMBERS',()=>{
     var result = util.sum(2,4);
     var expected = 6;
 
     // jest in action now
+
+    expect(result).toBe(expected); // A test case
+})
+*/
+
+// 2.Adding item to a list test case
+
+test('ADDING ITEM TO A LIST MUTABLY',()=>{
+    var list = ['redux','react-router'];
+    // result will come from our file and then we'll test it with our expected result using jest
+    var result = util.addItemToList(list,'react-modal');
+
+    var expected = ['redux','react-router','react-modal'];
+
+    expect(result).toEqual(expected);
+})
