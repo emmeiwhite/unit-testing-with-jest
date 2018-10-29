15e24acf1c715d99d1f4efa92be6e3c09d6cfb50
emmeiwhite <emmeiwhite@gmail.com>
2018-10-29 15:26:10 +0530

    by deep-freeze module we freeze an array to be modified directly"

--- util.js
+++ util.js
@@ -2,16 +2,24 @@
 const sum = (num1,num2)=>{
     return num1+num2;
 }
-
-
-// 2. Adding Item to a list
+// CRUD operations being tested
+// 1. Adding Item to a list
 const addItemToList = (list,listItem)=>{
     // a) In a mutable way we are adding an item to a list
     list.push(listItem);
     return list;
+    // return list.concat(listItem);
+}
+
+const removeItemFromList = (list,index)=>{
+    // return list.splice(index,1)
+}
+
+const updateItemFromList = (list,index,listItem)=>{
+    return list.splice(index,1)
 }
 
 module.exports = {
-    // sum
+    sum,
     addItemToList
 }
--- util.test.js
+++ util.test.js
@@ -1,8 +1,10 @@
 let util =require('./util.js'); // All util file comes here 
 
+let deepFreeze = require('deep-freeze');
+// A) ARITHMETIC OPERATIONS - JEST TESTING
 
 // 1.Adding two numbers
-/*
+
 test('ADDING TWO NUMBERS',()=>{
     var result = util.sum(2,4);
     var expected = 6;
@@ -11,12 +13,15 @@ test('ADDING TWO NUMBERS',()=>{
 
     expect(result).toBe(expected); // A test case
 })
-*/
 
-// 2.Adding item to a list test case
+// B) CRUD OPERATIONS -jest Testing
 
-test('ADDING ITEM TO A LIST MUTABLY',()=>{
+// 1.Adding item to a list test case
+
+test('ADDING ITEM TO A LIST IMMUTABLY',()=>{
     var list = ['redux','react-router'];
+    
+    deepFreeze(list)// Object is freeded now  AND cannot be immutated  directly  anymore                                                                                       
     // result will come from our file and then we'll test it with our expected result using jest
     var result = util.addItemToList(list,'react-modal');
 
